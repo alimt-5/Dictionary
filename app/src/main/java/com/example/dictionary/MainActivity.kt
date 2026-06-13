2558c2154b73a88cb8ec204601c1e0529fb300f8
Alireza <motlaghalireza943@gmail.com>
2026-06-14 00:34:00 +0330

    Fixed All Bug

--- app/src/main/java/com/example/dictionary/MainActivity.kt
+++ app/src/main/java/com/example/dictionary/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.*
 import androidx.compose.runtime.LaunchedEffect
